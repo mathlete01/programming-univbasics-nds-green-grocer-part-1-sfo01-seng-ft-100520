1e43a623eae7ad6f555209237b363faf1677f3ee
Matthew Sallin <msallin@gmail.com>
2020-10-02 03:38:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -37,6 +37,7 @@ def consolidate_cart(cart)
   newCart = []
   i = 0
   while i < cart.length
+    binding.pry
     new_cart_item = find_item_by_name_in_collection(cart[i][:item], newCart)
     if new_cart_item != nil
       new_cart_item[:count] += 1
