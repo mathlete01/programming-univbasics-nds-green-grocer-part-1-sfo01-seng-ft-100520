d6286743a043d327ecf282da1046ce8ff3f7e400
Matthew Sallin <msallin@gmail.com>
2020-10-02 03:35:12 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,51 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+def find_item_by_name_in_collection(name, collection)
+  i = 0
+  while i < collection.length
+    if collection[i][:item] == name
+      return collection[i]
+    end
+    i += 1
+  end
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  new_cart = []
+  counter = 0
+  while counter < cart.length
+    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+    if new_cart_item != nil
+      new_cart_item[:count] += 1
+    else
+      new_cart_item = {
+        :item => cart[counter][:item],
+        :price => cart[counter][:price],
+        :clearance => cart[counter][:clearance],
+        :count => 1
+      }
+      new_cart << new_cart_item
+    end
+    counter += 1
+  end
+  new_cart
 end
 
-
-  
+=begin
+def consolidate_cart(cart)
+  i = 0
+  newCart = []
+  while i < cart.length
+    currentName = cart[i][:item]
+    eval = find_item_by_name_in_collection(currentName,newCart)
+    if eval != nil
+      newCart[i][:count] += 1
+    else
+      newCart.push(cart[i])
+      newCart[i][:count] = 1
+    end
+  i += 1
+  end
+  newCart
+end
+=end
